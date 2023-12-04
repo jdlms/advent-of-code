692a50c100cb5795cbf1b29270045d331c2f065c
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2023-12-04 20:13:37 +0100

    day 1 refactor

--- 1/1.go
+++ 1/1.go
@@ -6,7 +6,58 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
+
+func trebuchet(input []string, puzzle int) int {
+
+	if puzzle != 1 && puzzle != 2 {
+		fmt.Println("Please enter numbers 1 or 2")
+		return -1
+	}
+
+	finalArr := make([]int, 0)
+	regex := regexp.MustCompile(`\D`)
+	spelledOut := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	for _, s := range input {
+		if puzzle == 2 {
+			for i, num := range spelledOut {
+				replacement := num + strconv.Itoa(i+1) + num
+				s = strings.Replace(s, num, replacement, -1)
+			}
+		}
+
+		var arr []string
+		intString := regex.ReplaceAllString(s, "")
+		for i, r := range intString {
+			if i == 0 || i == len(intString)-1 {
+				toString := string(r)
+				arr = append(arr, toString)
+			}
+		}
+
+		if len(arr) > 1 {
+			n, err := strconv.Atoi(string(arr[0]) + string(arr[1]))
+			if err != nil {
+				fmt.Println(err)
+			}
+			finalArr = append(finalArr, n)
+		} else {
+			n, err := strconv.Atoi(string(arr[0] + arr[0]))
+			if err != nil {
+				panic(err)
+			}
+			finalArr = append(finalArr, n)
+		}
+	}
+	sum := 0
+	for _, arr := range finalArr {
+		sum += arr
+	}
+	fmt.Println(sum)
+	return sum
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -15,39 +66,16 @@ func main() {
 	}
 	defer file.Close()
 
-	regex := regexp.MustCompile(`\D`)
-	digits := make([][]string, 0)
+	var input []string
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		stringNum := regex.ReplaceAllString(line, "")
-
-		var arr []string
-
-		for i, n := range stringNum {
-			if i == 0 || i == len(stringNum)-1 {
-				arr = append(arr, string(n))
-			}
-		}
-		digits = append(digits, arr)
+		input = append(input, line)
 	}
-
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
-
-	totalValue := 0
-	for _, pair := range digits {
-		if len(pair) == 1 {
-			pair = append(pair, pair[0])
-		}
-		num, err := strconv.Atoi(pair[0] + pair[1])
-		if err != nil {
-			panic(err)
-		}
-		totalValue += num
-	}
-
-	fmt.Println(totalValue)
+	// enter 1 or 2 for each puzzle solution
+	trebuchet(input, 2)
 }
