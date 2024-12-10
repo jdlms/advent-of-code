31afcfe59e4ba225ef73f7c3e6d5939f9057ac13
joshua-lamorey-statista <joshua.lamorey@statista.com>
2024-12-10 17:02:54 +0100

    part one, day one

--- 2024/1/main.ts
+++ 2024/1/main.ts
@@ -1,4 +1,39 @@
 const data = await Deno.readTextFile("input.txt");
 
 const lines = data.split("\n");
-console.log(lines);
+
+// const arr = ["343   4", "4   3", "2   5", "15   3", "321   9", "3   3"]
+
+const partOne = function (lines: string[]) {
+    let total = 0
+
+    const firstArr: number[] = []
+    const secondArr: number[] = []
+
+    lines.map((item) => {
+        const newArray = item.split(" ").filter(e => e).map(e => Number(e))
+        firstArr.push(newArray[0])
+        secondArr.push(newArray[1])
+        return
+    });
+
+    firstArr.sort((a, b) => a - b)
+    secondArr.sort((a, b) => a - b)
+
+    firstArr.map((item, i) => {
+        if (item > secondArr[i]) {
+            const int = item - secondArr[i]
+            total += int
+            return
+        } else if (secondArr[i] > item) {
+            const int = secondArr[i] - item
+            total += int
+        }
+
+    })
+
+    return total
+}
+
+
+console.log(partOne(lines));
